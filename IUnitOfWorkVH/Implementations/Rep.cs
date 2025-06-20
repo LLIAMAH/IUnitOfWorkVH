d33473188c96b75d089b8c8678294fec24abd2a3
Vasily Himchenko <White_Scorpion@inbox.lv>
2025-06-21 02:56:49 +0300

    Added interfaces: - IRepBase<T>, IRep<T>, IUnitOfWork and implementations - RepBase<T>, Rep<T> and abstract AbstractUnitOfWork<T>

---
+++ IUnitOfWorkVH/Implementations/Rep.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using IUnitOfWorkVH.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IUnitOfWorkVH.Implementations
+{
+    public class RepBase<T>(DbContext ctx) : IRepBase<T>
+        where T : class
+    {
+        protected readonly DbContext _ctx = ctx;
+        protected readonly DbSet<T> _dbSet = ctx.Set<T>();
+
+        public IQueryable<T> Get(Expression<Func<T, bool>>? filter = null, string? include = null,
+            bool asNoTracking = false)
+        {
+            IQueryable<T> query = _dbSet;
+            if (asNoTracking)
+                query = query.AsNoTracking();
+
+            if (!string.IsNullOrEmpty(include))
+                query = include.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Aggregate(query, (current, inc) => current.Include(inc.Trim()));
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            return query;
+        }
+    }
+
+    public class Rep<T>(DbContext ctx) : RepBase<T>(ctx), IRep<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
---
+++ IUnitOfWorkVH/Implementations/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using IUnitOfWorkVH.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace IUnitOfWorkVH.Implementations
+{
+    public abstract class AbstractUnitOfWork<T> : IUnitOfWork where T : DbContext
+    {
+        protected T _ctx;
+        protected Logger<T> _logger;
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var result = this._ctx.SaveChangesAsync(cancellationToken);
+                return result;
+            }
+            catch(Exception ex)
+            {
+                this._logger?.LogError(ex, ex.Message);
+                return Task.FromResult(0);
+            }
+        }
+
+        public int SaveChanges()
+        {
+            try
+            {
+                var result = this._ctx.SaveChanges();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                this._logger?.LogError(ex, ex.Message);
+                return 0;
+            }
+        }
+
+        public void Dispose()
+        {
+            this._ctx?.Dispose();
+        }
+    }
+}
