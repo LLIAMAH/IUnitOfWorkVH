f20f807310bf0a097201b734f068e3dca6848a8a
Vasily Himchenko <White_Scorpion@inbox.lv>
2025-11-14 12:55:48 +0200

    covered warning

--- IUnitOfWorkVH.Tests/Models/ShopItem.cs
+++ IUnitOfWorkVH.Tests/Models/ShopItem.cs
@@ -16,6 +16,8 @@ namespace IUnitOfWorkVH.Tests.Models
 
         [ForeignKey(nameof(ShopItemType))]
         public long ShopItemTypeId { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         public virtual ShopItemType ShopItemType { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     }
 }
